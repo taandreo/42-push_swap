3020b2834c800217b6d26429c11a7ff7bbe5e65e
Tairan Andreo <taandreo@hotmail.com>
2023-08-17 00:44:01 +0000

    adding bonus in Makefile

--- Makefile
+++ Makefile
@@ -2,6 +2,7 @@
 
 CC = cc
 NAME = push_swap
+BONUS = checker
 LIBFT = ./lib/libft.a
 LIBFT_DIR = ./libft
 CFLAGS = -Wall -Wextra -Werror
@@ -12,27 +13,36 @@ ifeq ($(UNAME), Darwin)
 	CFLAGS = -Wall -Wextra -Werror -arch x86_64
 endif
 
-SRCS = index.c stack_0.c stack_1.c push_swap.c utils.c markup.c parse.c moves_0.c moves_1.c solve_b_to_a_0.c solve_b_to_a_1.c solve_a_to_b.c solve.c prints.c
+SRCS = index.c stack_0.c stack_1.c utils.c markup.c \
+       parse.c moves_0.c moves_1.c solve_b_to_a_0.c \
+	   solve_b_to_a_1.c solve_a_to_b.c solve.c prints.c
 
-OBJS = $(SRCS:.c=.o)
+SRCS_MAIN  = push_swap.c $(SRCS)
+SRCS_BONUS = checker.c $(SRCS)
+
+OBJS_MAIN  = $(SRCS_MAIN:.c=.o)
+OBJS_BONUS = $(SRCS_BONUS:.c=.o)
 
 all: $(NAME)
 
 %.o: %.c
 	$(CC) $(CFLAGS) -I libft/include -I include -c $< -o $@ 
 
-$(NAME): $(OBJS) | libft
-	$(CC) $(CFLAGS) $(OBJS) -o $(NAME) $(LIBS) -L $(LIBFT_DIR)
+$(NAME): $(OBJS_MAIN) | libft
+	$(CC) $(CFLAGS) $(OBJS_MAIN) -o $(NAME) $(LIBS) -L $(LIBFT_DIR)
+
+bonus: $(OBJS_BONUS) | libft
+	$(CC) $(CFLAGS) $(OBJS_BONUS) -o $(BONUS) $(LIBS) -L $(LIBFT_DIR)
 
 libft:
 	make -C $(LIBFT_DIR)
 
 clean:
-	rm -f $(OBJS)
+	rm -f $(OBJS_MAIN) $(OBJS_BONUS)
 	make clean -C $(LIBFT_DIR)
 
 fclean: clean
-	rm -f $(NAME) $(LIBFT)
+	rm -f $(NAME) $(BONUS) $(LIBFT) 
 	make fclean -C $(LIBFT_DIR)
 
 re: fclean all
---
+++ checker.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   checker.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tairribe <tairribe@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/08/17 00:24:00 by tairribe          #+#    #+#             */
+/*   Updated: 2023/08/17 00:43:41 by tairribe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+t_push_swap	*init_push_swap(void)
+{
+	t_push_swap	*ps;
+
+	ps = ft_calloc(sizeof(t_push_swap), 1);
+	if (ps == NULL)
+		return (NULL);
+	ps->stack_a = ft_calloc(sizeof(t_dcll), 1);
+	ps->stack_b = ft_calloc(sizeof(t_dcll), 1);
+	ps->moves = NULL;
+	ps->moves_gt = NULL;
+	ps->words = NULL;
+	return (ps);
+}
+
+
+int	main(int argc, char *argv[])
+{
+	t_push_swap	*ps;
+
+	if (argc < 2)
+		return (0);
+	ps = init_push_swap();
+	parse_argv(ps, argc, argv);
+    
+}
